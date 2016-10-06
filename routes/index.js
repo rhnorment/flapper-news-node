2647623ba47c028ee097630ed8f6a34599103651
Hunt Norment <hunt@sherpa.red>
2016-10-06 16:01:54 -0500

    include comments in query return

--- routes/index.js
+++ routes/index.js
@@ -39,8 +39,24 @@ router.param('post', function(req, res, next, id) {
 	});
 });
 
-router.get('/posts/:post', function(req, res) {
-	res.json(req.post);
+router.param('comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function (err, comment) {
+		if(err) { return next(err); }
+		if(!comment) { return next(new Error('can\'t find comment')); }
+
+		req.comment = comment;
+		return next();
+	});
+});
+
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -66,5 +82,15 @@ router.post('/posts/:post/comments', function(req, res, next) {
 		});
 	});
 });
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+
 
 module.exports = router;
