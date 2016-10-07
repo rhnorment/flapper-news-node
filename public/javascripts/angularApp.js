496a8afbc1d10f9324f95afb5e6ff444e76f6bcb
Hunt Norment <hunt@sherpa.red>
2016-10-07 15:32:43 -0500

    upvote comments in browser

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -20,7 +20,12 @@ function($stateProvider, $urlRouterProvider) {
     .state('posts', {
     	url: '/posts/{id}',
     	templateUrl: '/posts.html',
-    	controller: 'PostsCtrl'
+    	controller: 'PostsCtrl',
+    	resolve: {
+    		post: ['$stateParams', 'posts', function($stateParams, posts) {
+    			return posts.get($stateParams.id);
+    		}]
+    	}
     });
 
   $urlRouterProvider.otherwise('home');
@@ -44,8 +49,24 @@ app.factory('posts', ['$http', function($http) {
 	};
 
 	o.upvote = function(post) {
-		return $http.put('/posts/' + post._id + '/upvote').success(function(data){
+		return $http.put('/posts/' + post._id + '/upvote').success(function(data) {
 			post.upvotes += 1;
+		});
+	};
+
+	o.get = function(id) {
+		return $http.get('/posts/' + id).then(function(res) {
+			return res.data;
+		});
+	};
+
+	o.addComment = function(id, comment) {
+		return $http.post('/posts/' + id + '/comments', comment);
+	};
+
+	o.upvoteComment = function(post, comment) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function(data){
+			comment.upvotes += 1;
 		});
 	};
 
@@ -77,20 +98,26 @@ function($scope, posts) {
 
 app.controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts) {
-	$scope.post = posts.posts[$stateParams.id];
+'post',
+function($scope, posts, post) {
+	$scope.post = post;
 
 	$scope.addComment = function(){
   	if($scope.body === '') { return; }
-  	$scope.post.comments.push({
-    	body: $scope.body,
-    	author: 'user',
-    	upvotes: 0
+  	
+  	posts.addComment(post._id, {
+  		body: $scope.body,
+  		author: 'user',
+  	}).success(function(comment) {
+  		$scope.post.comments.push(comment);
   	});
   	
   	$scope.body = '';
+	};
+
+	$scope.incrementUpvotes = function(comment) {
+		posts.upvoteComment(post, comment);
 	};
 }]);
 
