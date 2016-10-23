f2f4f533cc169bb8df17033e9b778270e20020ee
Hunt Norment <hunt@sherpa.red>
2016-10-23 10:49:03 -0500

    install express-jwt

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
+
 mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
@@ -29,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(passport.initialize());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -91,6 +93,36 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   });
 });
 
+router.post('/register', function(req, res, next) {
+	if (!req.body.username || !req.body.password) {
+		return res.status(400).json({ message: 'Please fill out all fields' });
+	}
 
+	var user = new User();
+	var username = req.body.username;
+	user.setPassword(req.body.password) // Might need semi-colon
+
+	user.save(function (err) {
+		if (err) { return next(err); }
+
+		return res.json({ token: user.generateJWT() })
+	});
+});
+
+router.post('/login', function(req, res, next) {
+	if (!request.body.username || !request.body.password) {
+		return res.status(400).json({ message: 'Please fill out all fields' });
+	}
+
+	passport.authenticate('local', function(err, user, info) {
+		if (err) { return next(err); }
+
+		if (user) {
+			return res.json({ token: user.generateJWT() });
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res, next);
+});
 
 module.exports = router;
