11e2144c4bf6968481f5f14897bc6d92b418052b
Hunt Norment <hunt@sherpa.red>
2016-11-16 15:01:04 -0600

    finish frontend

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+	$urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	};
+
+	return o;
+}]);
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope) {
-	$scope.posts = [];
+'posts',
+function($scope, posts) {
+	$scope.posts = posts.posts;
 
 	$scope.addPost = function() {
 		if (!$scope.title || $scope.title === '') { return; }
@@ -11,7 +40,11 @@ function($scope) {
 		$scope.posts.push({ 
 			title: $scope.title, 
 			link: $scope.link,
-			upvotes: 0 
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    		{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+			] 
 		});
 
 		$scope.title = '';
@@ -20,5 +53,29 @@ function($scope) {
 
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
+	};
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function() {
+		if ($scope.body === '') { return; }
+
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+
+		$scope.body = '';
+	};
+
+	$scope.incrementUpvotes = function(comment) {
+		comment.upvotes += 1;
 	};
 }]);
